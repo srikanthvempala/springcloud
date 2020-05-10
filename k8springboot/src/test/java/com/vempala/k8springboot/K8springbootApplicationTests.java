fe72c5d60c1313f82304c4b8f71446a0b52b9df8
srikanthvempala <srithirumala999@gmail.com>
2020-05-10 20:27:05 +0530

    added new dir

---
+++ k8springboot/src/test/java/com/vempala/k8springboot/K8springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vempala.k8springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
